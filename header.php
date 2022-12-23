0de5bc98e341cd24807d55d10c9dd534db4ada74
Akshaya P <115099585+akshayap97@users.noreply.github.com>
2022-12-23 12:36:46 +0530

    Add files via upload

---
+++ header.php
@@ -0,0 +1,26 @@
+ <div id="top-bar" class="transparent dark">
+
+                <div id="logo">
+				<img src="images/logo/logoa.png">
+                  <a class="logohead" href="index.php">Akshaya P</a> 
+                </div>
+
+                <nav>
+                    <ul id="mainmenu">
+                        <li><a class="btnhome" href="index.php">Home</a></li>
+                        
+                        
+                      
+                    </ul>
+                </nav>
+
+                <span id="menu-btn">
+					<span class="lt"></span>
+					<span class="lm"></span>
+					<span class="lb"></span>
+				</span>
+
+
+                <div class="clearfix"></div>
+
+            </div>
---
+++ sidebar.php
@@ -0,0 +1,12 @@
+  
+
+
+  <div class="social-icons-fixed">
+                    <div class="sc-inner">
+                        <a target="blank" href="https://www.facebook.com/profile.php?id=100004593435995"><i class="fa fa-facebook fa-lg"></i></a>
+                        <a target="blank" href="https://www.instagram.com/aksh_____aya/"><i class="fa fa-instagram fa-lg"></i></a>
+                        <a target="blank" href="https://twitter.com/achu128822"><i class="fa fa-twitter fa-lg"></i></a>
+                        <a target="blank" href="https://www.linkedin.com/in/akshaya-p-587767168"><i class="fa fa-linkedin fa-lg"></i></a>
+                        <a target="blank" href="mailto:achu128822@gmail.com"><i class="fa fa-envelope-o fa-lg"></i></a>
+                    </div>
+                </div>
